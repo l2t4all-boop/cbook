620ab497f2f6cfaf4c880b3ee2e9a3808546c791
l2t <l2t@gmail.com>
2026-02-09 19:10:56 +0530

    Added basic configurations.

--- src/main/java/com/l2t/cbook/CbookBeAppApplication.java
+++ src/main/java/com/l2t/cbook/CbookBeAppApplication.java
@@ -3,11 +3,11 @@ package com.l2t.cbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class CbookBeAppApplication {
+public class CbookBeAppApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CbookBeAppApplication.class, args);
 	}
-
 }
