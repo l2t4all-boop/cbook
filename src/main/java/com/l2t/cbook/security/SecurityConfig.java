4aa9708bb8854dc5e1712b727c23e0feef2ed8f5
l2t <l2t@gmail.com>
2026-02-14 11:29:36 +0530

    Changed ui url configuration.

--- src/main/java/com/l2t/cbook/security/SecurityConfig.java
+++ src/main/java/com/l2t/cbook/security/SecurityConfig.java
@@ -51,8 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500",
-                "http://localhost:3000", "http://127.0.0.1:3000"));
+        config.addAllowedOrigin("*");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
